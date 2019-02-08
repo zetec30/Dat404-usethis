f5a755911ebf70266f8250b3e87a91671d388875
zetec30 <44362077+zetec30@users.noreply.github.com>
2019-02-08 17:28:12 +0000

    new update 8/2/19
    update

---
+++ Dat404-usethis-master/js/script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  currentYear();
+};
+
+function currentYear(){
+  const date = new Date();
+  const autoDate = document.querySelector('#autoDate');
+  autoDate.textContent = date.getFullYear();
+};
