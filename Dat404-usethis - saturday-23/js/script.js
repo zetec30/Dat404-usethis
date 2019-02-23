f97f326ef15b209fa57a88a6725c10f2a4969c78
zetec30 <44362077+zetec30@users.noreply.github.com>
2019-02-23 08:15:20 +0000

    saturday 23rd
    near finish

---
+++ Dat404-usethis - saturday-23/js/script.js
@@ -0,0 +1,220 @@
+//facebook logo from black to blue on mouse over.
+function mouseOver() {
+    if (window.document.face.src = 'assets/13962.png') {
+        window.document.face.src = 'assets/13962-onmouse.png';
+
+    }
+}
+
+function mouseOut() {
+    if (window.document.face.src = 'assets/13962-onmouse.png') {
+        window.document.face.src = 'assets/13962.png';
+
+    }
+}
+
+
+//instagram logo from black to blue on mouse over.
+function mouseOveri() {
+    if (document.insta.src = 'assets/insta.png') {
+        document.insta.src = 'assets/insta-mouseover.png';
+
+    }
+}
+
+function mouseOuti() {
+    if (document.insta.src = 'assets/insta-mouseover.png') {
+        document.insta.src = 'assets/insta.png';
+
+    }
+}
+
+
+
+//arcustom logo mouse over
+function bigar() {
+    if (document.ar.src = 'assets/ar.PNG') {
+        document.ar.src = 'assets/ar-big.png';
+
+    }
+
+}
+
+//arcustom mouse out
+function litar() {
+    if (document.ar.src = 'assets/ar-big.png') {
+        document.ar.src = 'assets/ar.PNG';
+    }
+}
+
+
+
+
+
+function bus1() {
+    if (document.bus.src = 'assets/Capture.png') {
+        document.bus.src = 'assets/businessCard.png';
+
+    }
+
+}
+
+
+function bus2() {
+    if (document.bus.src = 'assets/businessCard.png') {
+        document.bus.src = 'assets/Capture.png';
+    }
+}
+
+function irep1() {
+    if (document.irep.src = 'assets/irepair.png') {
+        document.irep.src = 'assets/irepair2.png';
+
+    }
+
+}
+
+
+function irep2() {
+    if (document.irep.src = 'assets/irepair2.png') {
+        document.irep.src = 'assets/irepair.png';
+    }
+}
+
+function char1() {
+    if (document.char.src = 'assets/char.png') {
+        document.char.src = 'assets/char2.png';
+
+    }
+
+}
+
+
+function char2() {
+    if (document.char.src = 'assets/char2.png') {
+        document.char.src = 'assets/char.png';
+    }
+}
+
+function glitch1() {
+    if (document.glitch.src = 'assets/glitch.png') {
+        document.glitch.src = 'assets/glitch2.png';
+
+    }
+
+}
+
+
+function glitch2() {
+    if (document.glitch.src = 'assets/glitch2.png') {
+        document.glitch.src = 'assets/glitch.png';
+    }
+}
+
+function eye1() {
+    if (document.eye.src = 'assets/eye.png') {
+        document.eye.src = 'assets/eye2.png';
+
+    }
+
+}
+
+
+function eye2() {
+    if (document.eye.src = 'assets/eye2.png') {
+        document.eye.src = 'assets/eye.png';
+    }
+}
+
+function six1() {
+    if (document.sixty.src = 'assets/60.png') {
+        document.sixty.src = 'assets/602.png';
+
+    }
+
+}
+
+
+function six2() {
+    if (document.sixty.src = 'assets/602.png') {
+        document.sixty.src = 'assets/60.png';
+    }
+}
+
+function fog1() {
+    if (document.fury.src = 'assets/fury.png') {
+        document.fury.src = 'assets/fury2.png';
+
+    }
+
+}
+
+
+function fog2() {
+    if (document.fury.src = 'assets/fury2.png') {
+        document.fury.src = 'assets/fury.png';
+    }
+}
+
+
+
+function dmn1() {
+    if (document.dmn.src = 'assets/dmn.png') {
+        document.dmn.src = 'assets/dmn2.png';
+
+    }
+
+}
+
+
+function dmn2() {
+    if (document.dmn.src = 'assets/dmn2.png') {
+        document.dmn.src = 'assets/dmn.png';
+    }
+}
+
+function air1() {
+    if (document.airsoft.src = 'assets/airsoft.png') {
+        document.airsoft.src = 'assets/airsoft2.png';
+
+    }
+
+}
+
+
+function air2() {
+    if (document.airsoft.src = 'assets/airsoft2.png') {
+        document.airsoft.src = 'assets/airsoft.png';
+    }
+}
+
+
+function corn1() {
+    if (document.cornwall.src = 'assets/cornwall.png') {
+        document.cornwall.src = 'assets/cornwall2.png';
+
+    }
+
+}
+
+
+function corn2() {
+    if (document.cornwall.src = 'assets/cornwall2.png') {
+        document.cornwall.src = 'assets/cornwall.png';
+    }
+}
+
+function cust1() {
+    if (document.cust.src = 'assets/arcustom.png') {
+        document.cust.src = 'assets/arcustom2.png';
+
+    }
+
+}
+
+
+function cust2() {
+    if (document.cust.src = 'assets/arcustom2.png') {
+        document.cust.src = 'assets/arcustom.png';
+    }
+}
